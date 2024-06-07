c008f21972113a401b13a4ac7cfba33a06970912
eljesa <eljesa@root.root>
2024-06-07 20:18:45 +0200

    Initialized empty project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javanetworking {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javanetworking to javafx.fxml;
+    exports com.example.javanetworking;
+}
