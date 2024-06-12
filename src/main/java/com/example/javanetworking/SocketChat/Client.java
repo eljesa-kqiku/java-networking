630151a0483ea91bdb798be0c31a5a64fcafd04e
eljesa-kqiku <eljesakqiku@gmail.com>
2024-06-12 06:06:06 +0200

    SocketChat (detyra 2): Added logic for the first step UI - selecting avatar and display name.

--- src/main/java/com/example/javanetworking/SocketChat/Client.java
+++ src/main/java/com/example/javanetworking/SocketChat/Client.java
@@ -4,18 +4,38 @@ import com.example.javanetworking.HelloApplication;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Client extends Application {
+    Stage primaryStage;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chat/name-avatar-screen.fxml"));
+        fxmlLoader.setControllerFactory(controllerClass -> {
+            try {
+                return controllerClass.getConstructor(com.example.javanetworking.SocketChat.Client.class).newInstance(this);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("SocketChat");
         stage.setScene(scene);
         stage.show();
+        primaryStage = stage;
+    }
+
+    public void setUser(String userData){
+        System.out.println(userData);
+        // TODO: to be implemented
+        VBox box = new VBox();
+        box.getChildren().add(new Label("second step"));
+        primaryStage.setScene(new Scene(box, 400, 400));
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/javanetworking/SocketChat/ClientUIController.java
+++ src/main/java/com/example/javanetworking/SocketChat/ClientUIController.java
@@ -1,15 +1,53 @@
 package com.example.javanetworking.SocketChat;
 
+import com.example.javanetworking.SocketChat.Model.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 
 public class ClientUIController {
+    private Client controller;
+    private  DropShadow dropShadow = new DropShadow();
+    private String selectedAvatar = "1";
+    @FXML
+    private Button av1, av2, av3;
     @FXML
     private TextField displayName;
 
+    private Button[] buttons = new Button[12];
+
+    public ClientUIController(Client c){
+        this.controller = c;
+    }
+    @FXML
+    private void initialize() {
+        buttons[0] = av1;
+        buttons[1] = av2;
+        buttons[2] = av3;
+
+
+        dropShadow.setInput(null); // Setting input to null achieves "three-pass-box" effect
+        dropShadow.setColor(javafx.scene.paint.Color.rgb(54,155,176));
+        dropShadow.setWidth(10);
+        dropShadow.setHeight(10);
+        dropShadow.setRadius(10);
+
+        buttons[Integer.parseInt(selectedAvatar) - 1].setEffect(dropShadow);
+    }
     @FXML
     protected void onSubmit() {
-        System.out.println("submit");
+        if(displayName.getText().isEmpty())
+            return;
+        controller.setUser(STR."\{displayName.getText()} | \{selectedAvatar}");
+    }
+    @FXML
+    protected void setSelectedAvatar(ActionEvent e){
+        String a = ((Button) e.getSource()).getId().substring(2);
+        buttons[Integer.parseInt(selectedAvatar) - 1].setEffect(null);
+        selectedAvatar = a;
+        buttons[Integer.parseInt(selectedAvatar) - 1].setEffect(dropShadow);
     }
 }
